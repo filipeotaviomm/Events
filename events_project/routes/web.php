5af02ba88e4d2b94f0bb8c8f97e20ebd6c1c64ee
Filipe <filipeotaviomm@gmail.com>
2024-03-26 12:03:27 -0300

    feat: create events

--- events_project/routes/web.php
+++ events_project/routes/web.php
@@ -1,18 +1,28 @@
 <?php
 
+use App\Http\Controllers\EventController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
+Route::post('/events', [EventController::class, 'store']);
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+
+
+
+
+Route::get('/produtos', function () {
+
+    $busca = request('search');
+
+    return view('products',  ['busca' => $busca]);
+});
+
+Route::get('/produto/{id?}', function ($id = null) {
+    return view('product', ['id' => $id]);
 });
